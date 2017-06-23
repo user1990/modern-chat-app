afd8dcef7cf25771e5919a298eaa77c1f2d85368
user1990 <paulius.rimg1990@gmail.com>
2017-06-23 10:15:09 +0300

    # Indentation

--- app/config/index.js
+++ app/config/index.js
@@ -3,8 +3,8 @@
 var init = function() {
 
   if (process.env.NODE_ENV === 'production') {
-    var redisURI 		= require('url').parse(process.env.REDIS_URL);
-    var redisPassword 	= redisURI.auth.split(':')[1];
+    var redisURI = require('url').parse(process.env.REDIS_URL);
+    var redisPassword = redisURI.auth.split(':')[1];
     return {
       db: {
         username: process.env.dbUsername,
